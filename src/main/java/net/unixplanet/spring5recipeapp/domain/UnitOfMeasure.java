d0d3b2f45af51fe4cd379f9f5a6cfa7c863ddeb0
Don Krause <dkrause@unixplanet.net>
2017-11-18 14:24:28 -0800

    First entities

---
+++ src/main/java/net/unixplanet/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package net.unixplanet.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMeasure() {
+        return description;
+    }
+
+    public void setMeasure(String measure) {
+        this.description = measure;
+    }
+}
